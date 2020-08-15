e03e76e4bf9122f3ad0da8f28cb9ecdb982a33e3
Dan Pelis <dppelis@gmail.com>
2020-08-15 16:13:44 -0400

    Added webhook endpoint

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template      
+from flask import Flask, render_template, request, Response   
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/webhook', methods=['POST'])
+def respond():
+    print(request.json);
+    return Response(status=200)
     
 
     
