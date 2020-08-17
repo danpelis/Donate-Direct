d242ae3459a769c6a418ade75ce87b111b03a03c
jessval <48558482+jessval@users.noreply.github.com>
2020-08-16 20:59:02 -0400

    Search and Registration form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template      
+#imports
+from flask import Flask, render_template, request, jsonify
 
+# Initialize Flask app
 app = Flask(__name__)
 
 @app.route("/")
